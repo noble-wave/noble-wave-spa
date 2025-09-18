a4ccf7b3ebf98412ba863d5f6d0c78f7ac19d68a
Nasre Alam <nasrealam407@gmail.com>
2025-09-18 16:03:16 +0530

    feat: add Privacy Policy component and routing; update footer links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { AboutComponent } from './about/about.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'detail', component: DetailsComponent}
+  { path: 'detail', component: DetailsComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
 
 @NgModule({
