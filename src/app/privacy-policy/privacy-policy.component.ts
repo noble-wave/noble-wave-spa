e72bce08e6f12c7188b9fa6b931a97e034d00f2f
Nasre Alam <nasrealam407@gmail.com>
2025-09-19 13:13:22 +0530

    feat: implement PrivacyPolicyComponent with scroll-to-top functionality

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss']
 })
-export class PrivacyPolicyComponent {}
+export class PrivacyPolicyComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Force scroll to top when component loads
+    window.scrollTo(0, 0);
+  }
+}
